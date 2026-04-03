5e31ddbe4a186edea67d9f4d4da684af88df3aaa
u137357 <85033680+BrumalLake@users.noreply.github.com>
2026-04-03 17:00:39 -0500

    improvements

--- ROBLOX_MULTI.cpp
+++ ROBLOX_MULTI.cpp
@@ -5,10 +5,16 @@
 
 int main()
 {
-	CreateMutex(NULL, TRUE, L"ROBLOX_singletonMutex"); //bInitialOwner is TRUE, takes ownership and holds it until this process exits
+	SetLastError(0);
+	HANDLE mutex = CreateMutex(NULL, TRUE, "ROBLOX_singletonMutex"); //bInitialOwner is TRUE, takes ownership and holds it until this process exits
+	if (GetLastError() == ERROR_ALREADY_EXISTS) {
+		printf("A client was opened prior to starting this program.\n"
+				"Opening future clients will close the first client but allow multiple to run afterwards.\n");
+		WaitForSingleObject(mutex, INFINITE);
+	}
 
-	printf("You can now use multiple ROBLOX clients. Closing this will cause all ROBLOX clients to terminate except for one.\n");
-	printf("Press any key to close");
-	getch();
+	printf("You can now use multiple ROBLOX clients. Closing this will cause all ROBLOX clients to terminate except for one.\n"
+			"Press any key to close");
+	_getch_nolock();
 	return 0;
 }
